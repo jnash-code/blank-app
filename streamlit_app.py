2fa8fd91a3530bf2cd5eb5054912716695244c92
jnash-code <111412746+jnash-code@users.noreply.github.com>
2024-10-02 19:44:23 +0000

    Update title again

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new great app")
+st.title("🎈 My brand new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
